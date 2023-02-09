8d6a1afb213a2ef4a70edfc2ca29ec77e6ad92ea
Ritu Raj Pradhan <riturajpradhan911@gmail.com>
2023-02-09 10:29:47 +0530

    Sign up is almost done.

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,3 +1,5 @@
+import { teal } from "@mui/material/colors";
+import { textAlign } from "@mui/system";
 import * as React from "react";
 // import Button from "@mui/material/Button";
 // import CssBaseline from "@mui/material/CssBaseline";
@@ -160,19 +162,56 @@ import * as React from "react";
 
 function SignUp(props) {
   return (
-    <div className="container-fluid">
+    <div className="container-fluid" >
       <div className="row mx-5">
         <div className="col-md-6">
           <div className="container-fluid px-0 landing-page-title">
-            <h1>Because Your Story Matters...</h1>
+            <h1 className="big-heading">Because your Story Matters...</h1>
             <h3 className="text-muted">
               Record your journey as you see fit, perhaps share it with the
               world.
             </h3>
           </div>
         </div>
-        <div className="col-md-6">
-          <div className="container-fluid px-0">
+        <div className="col-md-6 px-5">
+          <div className="signup-column col-lg-12 col-md-6">
+          <div className="container-fluid px-0 landing-page-title">
+              <div className="card set-colour">
+                <div className="card-header mx-auto signup-text">
+                  <h3>Sign Up</h3>
+                </div>
+                  <div className="card-body">
+                    <div class="container text-center">
+                      <div class="row">
+                        <div class="col">
+                          Column
+                        </div>
+                        <div class="col">
+                          Column
+                        </div>
+                      </div>
+                    </div>
+                    <div className="input-group flex-nowrap margin-between-input">
+                      <input type="text" className="form-control " placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping"></input>
+                    </div>
+
+                    <div className="input-group flex-nowrap margin-between-input">
+                      <input type="text" className="form-control" placeholder="Email" aria-label="Email" aria-describedby="addon-wrapping"></input>
+                    </div>
+
+                    <div className="input-group flex-nowrap set-colour outline-dark margin-between-input">
+                      <input type="text" className="form-control" placeholder="Password" aria-label="Password" aria-describedby="addon-wrapping"></input>
+                    </div>
+
+                    <div className= "text-center">
+                    <button className=" btn btn-lg btn-block btn-outline-dark set-signup-button-colour" type="button">Sign Up</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          {/* <div className="container-fluid px-0">
             <form>
               <div className="input-group">
                 <span className="input-group-text" id="basic-addon1">
@@ -190,7 +229,7 @@ function SignUp(props) {
               Record your journey as you see fit, perhaps share it with the
               world.
             </h3>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
