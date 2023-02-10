4b54b294d73e5bdea594f9ab297522401e7ace12
Ritu Raj Pradhan <riturajpradhan911@gmail.com>
2023-02-10 13:59:26 +0530

    hggh

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -5,7 +5,7 @@ import SignUp from "./SignUp";
 import SignIn from "./SignIn";
 
 function App() {
-  const [isLoggedIn, setLoggedIn] = useState(true);
+  const [isLoggedIn, setLoggedIn] = useState(false);
   const [isSignedUp, setIsSignedUp] = useState(true);
 
   function invertIsSignedUp(event) {
