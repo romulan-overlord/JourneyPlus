26bd8815314f4a6d9fb7383880552a168ea5e3de
Ritu Raj Pradhan <riturajpradhan911@gmail.com>
2023-02-14 14:39:46 +0530

    Authentication and Security almost done

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,24 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(session({
+  secret: 'my lil secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());    //passport manages the sessions
 
 mongoose.set("strictQuery", false);
 mongoose.connect("mongodb://localhost:27017/ProjectDB");
@@ -24,14 +36,19 @@ const userSchema = new mongoose.Schema({
   lastName: String,
   password: {
     type: String,
-    required: [true],
   },
   username: String,
   email: String,
   entries: [entrySchema],
 });
 
+userSchema.plugin(passportLocalMongoose);   //Used to hash and salt passwords and store users in our mongodb database
+
 const Users = mongoose.model("user", userSchema);
+
+passport.use(Users.createStrategy());
+passport.serializeUser(Users.serializeUser());
+passport.deserializeUser(Users.deserializeUser());
 
 app.get("/message", (req, res) => {
   res.json({ message: "Hello from server!" });
@@ -57,18 +74,32 @@ app.post("/submit-entry", (req, res) => {
 
 app.post("/signUp", (req, res) => {
   console.log(req.body);
-  const newUser = new Users({
-    ...req.body,
-    entries: [],
-  });
-  newUser.save();
-  res.send({ message: "success" });
+  Users.register({
+    firstName: req.body.firstName, 
+    lastName: req.body.lastName, 
+    username: req.body.username, 
+    email: req.body.email}, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      console.log("failure");
+    }else{
+      passport.authenticate("local")(req, res, function(){
+        console.log("success");
+      })
+    }
+  })
+  // console.log(req.body);
+  // const newUser = new Users({
+  //   ...req.body,
+  //   entries: [],
+  // });
+  // newUser.save();
+  // res.send({ message: "success" });
 });
 
 app.listen(8000, () => {
   console.log(`Server is running on port 8000.`);
 });
 
-// hey sham
-// jhgdjkdjk
+
 
--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -3,6 +3,36 @@ import { textAlign } from "@mui/system";
 import * as React from "react";
 
 function SignUp(props) {
+
+  function handleSubmitSignUp(event){
+    event.preventDefault();
+    //console.log("in handler");
+    const data = new FormData(event.currentTarget);
+    const requestData = {
+      firstName: data.get("firstName"),
+      lastName: data.get("lastName"),
+      username: data.get("username"),
+      email: data.get("email"),
+      password: data.get("password"),
+    };
+    fetch("http://localhost:8000/signUp", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        props.switch();
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+
+  }
+
   return (
     <div className="container-fluid center-div">
       <div className="row mx-5">
@@ -16,70 +46,53 @@ function SignUp(props) {
           </div>
         </div>
         <div className="col-md-6 px-5">
-          <div className="signup-column col-lg-12 col-md-6">
+          <div className="align-card signup-column col-lg-10 col-md-6">
             <div className="container-fluid px-0 landing-page-title">
                 <div className="card set-colour">
                   <div className="mx-auto signup-text">
                     <h3 className="card-title signup-text">Sign Up</h3>
                   </div>
-                    <div className="card-body">
-                      <div class="container text-center">
-                        <div class="row">
-                          <div class="col first-name-button">
+                  <div className="card-body">
+                  <form onSubmit={handleSubmitSignUp} method="POST">
+                    <div className="container text-center">
+                        <div className="row">
+                          <div className="col first-name-button">
                             <div className="input-group flex-nowrap margin-between-input">
-                              <input type="text" className="form-control " placeholder="First Name" aria-label="First Name" aria-describedby="addon-wrapping"></input>
+                              <input type="text" name="firstName" className="form-control " placeholder="First Name" aria-label="First Name" aria-describedby="addon-wrapping"></input>
                             </div>
                           </div>
-                          <div class="col last-name-button">
+                          <div className="col last-name-button">
                             <div className="input-group flex-nowrap margin-between-input">
-                              <input type="text" className="form-control " placeholder="Last Name" aria-label="Last Name" aria-describedby="addon-wrapping"></input>
+                              <input type="text" name="lastName" className="form-control " placeholder="Last Name" aria-label="Last Name" aria-describedby="addon-wrapping"></input>
                             </div>
                           </div>
                         </div>
                       </div>
-                      <div class="row">
+                      <div className="row">
                         <div className="input-text input-group flex-nowrap margin-between-input">
-                          <input type="text" className="form-control " placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping"></input>
+                          <input type="text" name="username" className="form-control " placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping"></input>
                         </div>
 
                         <div className="input-group flex-nowrap margin-between-input">
-                          <input type="text" className="form-control" placeholder="Email" aria-label="Email" aria-describedby="addon-wrapping"></input>
+                          <input type="text" name="email" className="form-control" placeholder="Email" aria-label="Email" aria-describedby="addon-wrapping"></input>
                         </div>
 
                         <div className="input-group flex-nowrap set-colour outline-dark margin-between-input">
-                          <input type="text" className="form-control" placeholder="Password" aria-label="Password" aria-describedby="addon-wrapping"></input>
+                          <input type="text" name="password" className="form-control" placeholder="Password" aria-label="Password" aria-describedby="addon-wrapping"></input>
                         </div>
                       </div>
                       <div className= "text-center">
-                      <button className="button-text btn btn-lg btn-block btn-outline-dark set-signup-button-colour" type="button">Sign Up</button>
+                      <button className="button-text btn btn-block btn-outline-dark set-signup-button-colour" type="submit">Sign Up</button>
                       </div>
+                  </form>
+                    
                       <div className= "text-center">
-                        <p>Already have an account?</p>
-                      </div>
+                        <p className="account-text">Already have an account? <a href="">Login</a></p>        
+                      </div> 
                     </div>
                   </div>
                 </div>
               </div>
-
-          {/* <div className="container-fluid px-0">
-            <form>
-              <div className="input-group">
-                <span className="input-group-text" id="basic-addon1">
-                  @
-                </span>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Username"
-                />
-              </div>
-            </form>
-            <h1>Because Your Story Matters...</h1>
-            <h3 className="text-muted">
-              Record your journey as you see fit, perhaps share it with the
-              world.
-            </h3>
-          </div> */}
         </div>
       </div>
     </div>
