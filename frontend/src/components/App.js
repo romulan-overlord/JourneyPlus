abad7ec7a617019c0e1c8387b2e77da97a9bddad
Ritu Raj Pradhan <riturajpradhan911@gmail.com>
2023-02-22 13:17:42 +0530

    start cookie code

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useCookies } from "react-cookie";
 
 import EntryInput from "./EntryInput/EntryInput";
 import Header from "./Header";
@@ -6,8 +7,15 @@ import SignUp from "./SignUp";
 import Login from "./Login";
 
 function App() {
-  const [isLoggedIn, setLoggedIn] = useState(true);
+  const [isLoggedIn, setLoggedIn] = useState(false);
   const [isSignedUp, setIsSignedUp] = useState(false);
+  const [currentUser, setCurrentUser] = useState({ test: false });
+
+  const [cookies, setCookies] = useCookies([
+    "userIsSaved",
+    "username",
+    "password",
+  ]);
 
   function invertIsSignedUp(event) {
     setIsSignedUp((prev) => {
@@ -21,13 +29,59 @@ function App() {
     });
   }
 
+  function updateCurrentUser(user) {
+    setCurrentUser(() => {
+      console.log("in setter");
+      return { ...user };
+    });
+    setCookies("userIsSaved", true);
+    setCookies("username", user.username);
+    setCookies("password", user.password);
+  }
+
+  console.log("wat is happen?" + typeof(cookies.userIsSaved));
+  // checking for cookies
+  if (cookies.userIsSaved === "true") {
+    console.log("checking cookies");
+    console.log(cookies.userIsSaved);
+    console.log(cookies.username);
+    console.log(cookies.password);
+    const requestData = {
+      username: cookies.username,
+      password: cookies.password,
+    };
+    fetch("http://localhost:8000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.success === "802") {
+          // console.log(data);
+          // props.updateCurrentUser(data.user);
+          console.log('it works!!!!!');
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+
   return (
     <div className="App height-100">
       <Header />
       {isLoggedIn ? (
         <EntryInput />
       ) : isSignedUp ? (
-        <Login invertLoggedIn={invertLoggedIn} switch={invertIsSignedUp} />
+        <Login
+          invertLoggedIn={invertLoggedIn}
+          switch={invertIsSignedUp}
+          updateCurrentUser={updateCurrentUser}
+        />
       ) : (
         <SignUp invertIsSignedUp={invertIsSignedUp} switch={invertIsSignedUp} />
       )}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import React from "react";
+import { CookiesProvider } from "react-cookie";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <CookiesProvider>
+      <App />
+    </CookiesProvider>
   </React.StrictMode>
 );
-
