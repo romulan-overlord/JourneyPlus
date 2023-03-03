3be6d0ddd5baebd201f933c3e92a64301c1cd3de
Ritu Raj Pradhan <riturajpradhan911@gmail.com>
2023-03-03 12:08:11 +0530

    card menu in progress

--- frontend/src/components/MainPage/Card.jsx
+++ frontend/src/components/MainPage/Card.jsx
@@ -1,34 +1,56 @@
 import React, { useState, useEffect } from "react";
 import PermMediaIcon from "@mui/icons-material/PermMedia";
+import AudiotrackIcon from "@mui/icons-material/Audiotrack";
 
 function Card(props) {
+  console.log(props.entry.media.video.length === 0);
 
-  console.log(props.entry.media.image.length);
-  const isMedia = () => {
+  const [checkFlags, setCheck] = useState(true);
+  const [flags, setFlags] = useState({
+    isMedia: false,
+    isBkgAud: false,
+  });
+
+  function checkMedia() {
+    let tempBool = true;
     if (
       props.entry.media.image.length === 0 &&
       props.entry.media.video.length === 0 &&
       props.entry.media.audio.length === 0
     )
-      return false;
-    return true;
-  };
+      tempBool = false;
+    else tempBool = true;
+    setFlags((prev) => {
+      return {
+        ...prev,
+        isMedia: tempBool,
+      };
+    });
 
-  const isBkgAud = () => {
-    if(entry.backgroundAudio === "" || entry.backgroundAudio === undefined)
-      return false;
-    return true;
+    if (props.entry.backgroundAudio === "" || props.entry.backgroundAudio === undefined)
+      tempBool = false;
+    tempBool =  true;
+    setFlags((prev) => {
+      return {
+        ...prev,
+        isBkgAud: tempBool,
+      };
+    });
   }
+
   useEffect(() => {
     const id = "#c" + props.index;
     const defaultImg = "./../images/img2.jpg";
     const imageID = props.entry.backgroundImage;
-    console.log(imageID);
     if (imageID === undefined || imageID === "")
       $(id).css("background-image", "url(" + defaultImg + ")");
     else {
       const imageUrl = "./../images/bkg" + imageID + ".jpg";
       $(id).css("background-image", "url(" + imageUrl + ")");
+    }
+    if(checkFlags === true){
+      checkMedia();
+      setCheck(false)
     }
   });
 
@@ -50,9 +72,14 @@ function Card(props) {
         </div>
         <div className="row w-100 bottom-menu">
           <div className="col-12 bottom-menu-col">
-            {isMedia ? (
+            {flags.isMedia ? (
               <span className="date-p">
                 <PermMediaIcon fontSize="small"></PermMediaIcon>
+              </span>
+            ) : null}
+            {flags.isBkgAud ? (
+              <span className="date-p">
+                <AudiotrackIcon fontSize="small"></AudiotrackIcon>
               </span>
             ) : null}
           </div>
